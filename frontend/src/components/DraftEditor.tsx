2411c2c3deb31ddc41b901dec2f1c0c1c6b65967
seongeun <whtjddms7@sju.ac.kr>
2024-10-23 12:17:31 +0900

    🚚 Rename : 모듈 재설치

---
+++ frontend/src/components/DraftEditor.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Editor } from 'react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import styled from 'styled-components'
+import { EditorState, convertToRaw } from 'draft-js'
+import { Component } from 'react'
+import draftToHtml from 'draftjs-to-html'
+
+const DraftEditor = () => {
+  return <div>DraftEditor</div>
+}
+
+export default DraftEditor
