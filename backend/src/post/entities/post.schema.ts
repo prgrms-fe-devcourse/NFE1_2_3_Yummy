d93b3f93cc5021775478bcf11fe24f2649d51911
HSCHEOL <knr0013@gmail.com>
2024-10-23 10:24:34 +0900

    🚀 Feat: post.schema 작성 및 수정

--- backend/src/post/entities/post.schema.ts
+++ backend/src/post/entities/post.schema.ts
@@ -1,16 +1,22 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({ timestamps: true })
 export class Post extends Document {
-  @Prop({ required: true })
-  title: string;
+  // @Prop({ required: true })
+  // user_id: string; // 회원가입 기능 완료 후 추가
 
   @Prop({ required: true })
-  content: string;
+  title: string; // 제목
+
+  @Prop({ required: true })
+  content: string; // 게시글 내용
 
   @Prop()
-  author: string;
+  category: string; // 카테고리
+
+  @Prop()
+  image_url: string; // 이미지 URL
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
