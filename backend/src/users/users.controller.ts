2d29094eec596d5b371c15e1107001645a7bbed5
HSCHEOL <knr0013@gmail.com>
2024-10-29 00:34:05 +0900

    🚀 Feat:회원 정보 수정 로직 구현

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,16 +1,31 @@
-import { Controller, Post, Body, UseGuards, Put, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Put,
+  Req,
+  Patch,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { UpdateProfileDto } from './dto/update-user.dto';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Put('/profile')
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '사용자 프로필 정보 수정' })
   @ApiResponse({
     status: 200,
@@ -18,9 +33,12 @@ export class UsersController {
   })
   @ApiResponse({ status: 404, description: 'User not found' })
   @ApiBody({ type: UpdateProfileDto, description: '수정할 프로필 정보' })
-  async updateProfile(@Req() req, @Body() updateProfileDto: UpdateProfileDto) {
+  async updateProfile(
+    @Req() req: any,
+    @Body() updateProfileDto: UpdateProfileDto,
+  ) {
     const userId = req.user._id;
-    console.log(userId);
+    console.log('User ID from JWT:', userId);
 
     return this.userService.updateProfile(userId, updateProfileDto);
   }
