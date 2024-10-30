a35bb5eef1fe29a766102d13c9b472677e61c63e
이영주 <lllucia@iyeongjuui-MacBookAir.local>
2024-10-30 12:55:28 +0900

    💄 Design: 메인 페이지 UI 수정

--- frontend/src/components/TopPost.tsx
+++ frontend/src/components/TopPost.tsx
@@ -8,16 +8,21 @@ const TopPostContainer = styled.div<{ $imgUrl: string }>`
   background-image: url(${(props) => props.$imgUrl});
   background-size: cover;
   background-position: center;
-  height: 300px;
+  width: 100%;
+  height: 600px;
   display: flex;
   align-items: flex-start;
 `
 
 const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   background-color: rgba(255, 255, 255, 1);
   padding: 16px;
-  margin: 16px;
-  max-width: 400px;
+  margin: 0 30px;
+  max-width: 500px;
+  height: 500px;
 `
 
 const PostCategory = styled.p`
@@ -41,6 +46,11 @@ const PostText = styled.p`
   font-size: 16px;
   color: #1c1c1c;
   margin-top: 8px;
+  display: -webkit-box;
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  text-overflow: ellipsis;
 `
 
 const TopPost: React.FC<Post> = ({
