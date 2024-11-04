5ff7320b2a2b9cd56c19c24536bc3ed02a53fcd5
seongeun <whtjddms7@sju.ac.kr>
2024-11-04 15:07:20 +0900

    💡 Comment : 주석 변경

--- frontend/src/hooks/useProfileImage.ts
+++ frontend/src/hooks/useProfileImage.ts
@@ -1,24 +1,23 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
-// 프로필 이미지 가져오기
+// query 함수
 const fetchProfileImage = async (userId: any) => {
   const { data } = await axios.get(`/api/user/${userId}`)
   return data.profileImageUrl
 }
 
 export const useProfileImage = () => {
-  // userId가 존재하는지 확인
   const userId = localStorage.getItem('userId')
 
-  // useQuery로 프로필 이미지 가져오기 (객체 형식으로 queryKey와 queryFn 명시)
+  // useQuery로 프로필 이미지 가져오기
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['profileImage', userId],
     queryFn: () => fetchProfileImage(userId),
-    enabled: !!userId, // userId가 있을 때만 쿼리를 실행
+    enabled: !!userId, // 쿼리 실행 조건
   })
 
-  // 에러가 발생할 경우 기본 아바타 URL을 사용하도록 설정
+  // 에러 처리 -> 기본 아바타 이미지
   const profileImageUrl = isError
     ? 'https://example.com/default-avatar.jpg'
     : data
